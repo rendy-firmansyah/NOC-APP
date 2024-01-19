807b8897c1fb1c2ed50f6bca2909b5d2e1140b41
Yoga <212410102031@mail.unej.ac.id>
2024-01-19 21:56:20 +0700

    feat final beta api form

---
+++ prisma/migrations/20240119144828_change_db_model/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `H` on the `kelompok_lansia` table. All the data in the column will be lost.
+  - You are about to drop the column `I` on the `kelompok_lansia` table. All the data in the column will be lost.
+  - You are about to drop the column `J` on the `kelompok_lansia` table. All the data in the column will be lost.
+  - You are about to drop the column `K` on the `kelompok_lansia` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "kelompok_lansia" DROP COLUMN "H",
+DROP COLUMN "I",
+DROP COLUMN "J",
+DROP COLUMN "K";
