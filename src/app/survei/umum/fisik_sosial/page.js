3a8dfff33f9e881c18c8960c069dd59fb8770e2f
AhmadRizkyFebianto <ahmadrizky13jkl@gmail.com>
2024-01-20 01:37:44 +0700

    fix : form lingkungan (radio button)

--- src/app/survei/umum/fisik_sosial/page.js
+++ src/app/survei/umum/fisik_sosial/page.js
@@ -118,43 +118,43 @@ export default function SurveiFisikSosial () {
                         <div>
                             <label>a. Kondisi ventilasi rumah:</label>
                             <div className="ms-5">
-                                <input type="radio" id="baik" value="Baik" onChange={(e)=> setA1(e.target.value)} /><label htmlFor="baik">Baik</label>
+                                <input type="radio" id="baik" name="kondisi_rumah"  value="Baik" onChange={(e)=> setA1(e.target.value)} /><label htmlFor="baik">Baik</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="cukup" value="Cukup" onChange={(e)=> setA1(e.target.value)}/><label htmlFor="cukup">Cukup</label>
+                                <input type="radio" id="cukup" name="kondisi_rumah" value="Cukup" onChange={(e)=> setA1(e.target.value)}/><label htmlFor="cukup">Cukup</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="kurang" value="Kurang" onChange={(e)=> setA1(e.target.value)}/><label htmlFor="kurang">Kurang</label>
+                                <input type="radio" id="kurang" name="kondisi_rumah" value="Kurang" onChange={(e)=> setA1(e.target.value)}/><label htmlFor="kurang">Kurang</label>
                             </div>
                         </div>
                         <div>
                             <label>b. Pencahayaan dalam rumah waktu siang hari:</label>
                             <div className="ms-5">
-                                <input type="radio" id="terang" value="Terang" onChange={(e)=> setA2(e.target.value)}/><label htmlFor="terang">Terang</label>
+                                <input type="radio" id="terang" name="pencahayaan" value="Terang" onChange={(e)=> setA2(e.target.value)}/><label htmlFor="terang">Terang</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="remang" value="Remang" onChange={(e)=> setA2(e.target.value)}/><label htmlFor="remang">Remang - remang</label>
+                                <input type="radio" id="remang" name="pencahayaan" value="Remang" onChange={(e)=> setA2(e.target.value)}/><label htmlFor="remang">Remang - remang</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="gelap" value="Gelap" onChange={(e)=> setA2(e.target.value)}/><label htmlFor="gelap">Gelap</label>
+                                <input type="radio" id="gelap" name="pencahayaan" value="Gelap" onChange={(e)=> setA2(e.target.value)}/><label htmlFor="gelap">Gelap</label>
                             </div>
                         </div>
                         <div>
                             <label>c. Kepadatan dan kebersihan lokasi atau area perumahan:</label>
                             <div className="ms-5">
-                                <input type="radio" id="padat" value="Padat" onChange={(e)=> setA3(e.target.value)}/><label htmlFor="padat">Padat</label>
+                                <input type="radio" id="padat" name="kepadatan" value="Padat" onChange={(e)=> setA3(e.target.value)}/><label htmlFor="padat">Padat</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="tidakPadat" value="TidakPadat" onChange={(e)=> setA3(e.target.value)}/><label htmlFor="tidakPadat">Tidak Padat</label>
+                                <input type="radio" id="tidakPadat" name="kepadatan" value="TidakPadat" onChange={(e)=> setA3(e.target.value)}/><label htmlFor="tidakPadat">Tidak Padat</label>
                             </div>
                         </div>
                         <div>
                             <label>d. Kondisi udara area perumahan:</label>
                             <div className="ms-5">
-                                <input type="radio" id="sejuk" value="Sejuk" onChange={(e)=> setA4(e.target.value)}/><label htmlFor="sejuk">Sejuk</label>
+                                <input type="radio" id="sejuk" name="kondisi_udara" value="Sejuk" onChange={(e)=> setA4(e.target.value)}/><label htmlFor="sejuk">Sejuk</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="panas" value="Panas" onChange={(e)=> setA4(e.target.value)}/><label htmlFor="panas">Panas</label>
+                                <input type="radio" id="panas" name="kondisi_udara" value="Panas" onChange={(e)=> setA4(e.target.value)}/><label htmlFor="panas">Panas</label>
                             </div>
                         </div>
                     </div>
@@ -165,25 +165,25 @@ export default function SurveiFisikSosial () {
                         <div>
                             <label>a. Kondisi air:</label>
                             <div className="ms-5">
-                                <input type="radio" id="berwarna" value="Berwarna" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="berwarna">Berwarna</label>
+                                <input type="radio" id="berwarna" name="kondisi_air" value="Berwarna" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="berwarna">Berwarna</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="berbau" value="Berbau" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="berbau">Berbau</label>
+                                <input type="radio" id="berbau" name="kondisi_air" value="Berbau" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="berbau">Berbau</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="berasa" value="Berasa" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="berasa">Berasa</label>
+                                <input type="radio" id="berasa" name="kondisi_air" value="Berasa" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="berasa">Berasa</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="tidakBerasa/Berbau" value="tidakBerasa/Berbau" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="tidakBerasa/Berbau">Tidak berasa/tidak berbau</label>
+                                <input type="radio" id="tidakBerasa/Berbau" name="kondisi_air" value="tidakBerasa/Berbau" onChange={(e)=> setB1(e.target.value)}/><label htmlFor="tidakBerasa/Berbau">Tidak berasa/tidak berbau</label>
                             </div>
                         </div>
                         <div>
                             <label>b. Terdapat/tidaknya jentik dalam tempat penampungan air:</label>
                             <div className="ms-5">
-                                <input type="radio" id="ya" value="Ya" onChange={(e)=> setB2(e.target.value)}/><label htmlFor="ya">Ya</label>
+                                <input type="radio" id="ya" value="Ya" name="jentik" onChange={(e)=> setB2(e.target.value)}/><label htmlFor="ya">Ya</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="tidak" value="Tidak" onChange={(e)=> setB2(e.target.value)}/><label htmlFor="tidak">Tidak</label>
+                                <input type="radio" id="tidak" value="Tidak" name="jentik" onChange={(e)=> setB2(e.target.value)}/><label htmlFor="tidak">Tidak</label>
                             </div>
                         </div>
                     </div>
@@ -194,31 +194,31 @@ export default function SurveiFisikSosial () {
                         <div>
                             <label>a. Dimanakah keluarga membuang sampah:</label>
                             <div className="ms-5">
-                                <input type="radio" id="Sungai" value="Sungai" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Sungai">Sungai</label>
+                                <input type="radio" id="Sungai" name="sampah" value="Sungai" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Sungai">Sungai</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="Ditimbun" value="Ditimbun" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Ditimbun">Ditimbun</label>
+                                <input type="radio" id="Ditimbun" name="sampah" value="Ditimbun" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Ditimbun">Ditimbun</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="Laut" value="Laut" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Laut">Laut</label>
+                                <input type="radio" id="Laut" name="sampah" value="Laut" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Laut">Laut</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="PetugasKebersihan" value="PetugasKebersihan" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="PetugasKebersihan">Petugas Kebersihan</label>
+                                <input type="radio" id="PetugasKebersihan" name="sampah" value="PetugasKebersihan" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="PetugasKebersihan">Petugas Kebersihan</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="Dibakar" value="Dibakar" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Dibakar">Dibakar</label>
+                                <input type="radio" id="Dibakar" name="sampah" value="Dibakar" onChange={(e)=> setC1(e.target.value)}/><label htmlFor="Dibakar">Dibakar</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="DisembarangTempat" value="DisembarangTempat"  onChange={(e)=> setC1(e.target.value)}/><label htmlFor="DisembarangTempat">Disembarang Tempat</label>
+                                <input type="radio" id="DisembarangTempat" name="sampah" value="DisembarangTempat"  onChange={(e)=> setC1(e.target.value)}/><label htmlFor="DisembarangTempat">Disembarang Tempat</label>
                             </div>
                         </div>
                         <div>
                             <label>b. Kebiasaan keluarga buang air besar:</label>
                             <div className="ms-5">
-                                <input type="radio" id="wc" value="WC" onChange={(e)=> setC2(e.target.value)}/><label htmlFor="wc">Jamban/WC</label>
+                                <input type="radio" id="wc" value="WC" name="bab" onChange={(e)=> setC2(e.target.value)}/><label htmlFor="wc">Jamban/WC</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="sungai" value="Sungai" onChange={(e)=> setC2(e.target.value)}/><label htmlFor="sungai">Sungai</label>
+                                <input type="radio" id="sungai" value="Sungai" name="bab" onChange={(e)=> setC2(e.target.value)}/><label htmlFor="sungai">Sungai</label>
                             </div>
                         </div>
                     </div>
@@ -229,31 +229,31 @@ export default function SurveiFisikSosial () {
                         <div>
                             <label>a. Adakah kegiatan sosial di masyarakat:</label>
                             <div className="ms-5">
-                                <input type="radio" id="Ya" value="Ya" onChange={(e)=> setD1(e.target.value)}/><label htmlFor="Ya">Ya</label>
+                                <input type="radio" id="Ya" value="Ya" name="kegiatan" onChange={(e)=> setD1(e.target.value)}/><label htmlFor="Ya">Ya</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="Tidak" value="Tidak" onChange={(e)=> setD1(e.target.value)}/><label htmlFor="Tidak">Tidak</label>
+                                <input type="radio" id="Tidak" value="Tidak" name="kegiatan" onChange={(e)=> setD1(e.target.value)}/><label htmlFor="Tidak">Tidak</label>
                             </div>
                         </div>
                         <div>
                             <label>b. Jenis kegiatan sosial di masyarakat:</label>
                             <div className="ms-5">
-                                <input type="radio" id="Pengajian" value="Pengajian" onChange={(e)=> setD2(e.target.value)}/><label htmlFor="Pengajian">Pengajian</label>
+                                <input type="radio" id="Pengajian" value="Pengajian" name="jenis_kegiatan" onChange={(e)=> setD2(e.target.value)}/><label htmlFor="Pengajian">Pengajian</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="Senam/OlahragaRutin" value="Senam/OlahragaRutin" onChange={(e)=> setD2(e.target.value)}/><label htmlFor="Senam/OlahragaRutin">Senam/Olahraga Rutin</label>
+                                <input type="radio" id="Senam/OlahragaRutin" value="Senam/OlahragaRutin" name="jenis_kegiatan" onChange={(e)=> setD2(e.target.value)}/><label htmlFor="Senam/OlahragaRutin">Senam/Olahraga Rutin</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="BaktiSosial" value="BaktiSosial"  onChange={(e)=> setD2(e.target.value)}/><label htmlFor="BaktiSosial">Bakti Sosial</label>
+                                <input type="radio" id="BaktiSosial" value="BaktiSosial" name="jenis_kegiatan" onChange={(e)=> setD2(e.target.value)}/><label htmlFor="BaktiSosial">Bakti Sosial</label>
                             </div>
                         </div>
                         <div>
                             <label>c. Apakah keluarga rutin menghadiri kegiatan di masyarakat:</label>
                             <div className="ms-5">
-                                <input type="radio" id="Ya" value="Ya" onChange={(e)=> setD3(e.target.value)}/><label htmlFor="Ya">Ya</label>
+                                <input type="radio" id="Ya" value="Ya" name="rutinan" onChange={(e)=> setD3(e.target.value)}/><label htmlFor="Ya">Ya</label>
                             </div>
                             <div className="ms-5">
-                                <input type="radio" id="Tidak" value="Tidak" onChange={(e)=> setD3(e.target.value)}/><label htmlFor="Tidak">Tidak</label>
+                                <input type="radio" id="Tidak" value="Tidak" name="rutinan" onChange={(e)=> setD3(e.target.value)}/><label htmlFor="Tidak">Tidak</label>
                             </div>
                         </div>
                     </div>
