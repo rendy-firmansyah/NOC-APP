acb050efdc76def92f9da08f911c012d9dd92a42
Yoga <212410102031@mail.unej.ac.id>
2024-01-21 08:26:00 +0700

    feat : using api in tambah wilayah page

--- src/app/Wilayah/page.js
+++ src/app/Wilayah/page.js
@@ -27,15 +27,21 @@ const Wilayah = () => {
     useEffect(() => {
         async function getData() {
             const fetch = await axios.get('/api/dashboard/wilayah')
-            setData(fetch.data.get_data)
+            setData(fetch.data.Data)
+            // console.log(fetch.data.Data)
         }
         getData()
     },[]) 
-    
+    async function getData() {
+        const fetch = await axios.get('/api/dashboard/wilayah')
+        setData(fetch.data.Data)
+        // console.log(fetch.data.Data)
+    }
+  
 
     async function add_wilayah(){
         const send = await axios.post("/api/dashboard/wilayah",sendData)
-        console.log(sendData)
+        console.log(send)
         if (send.data.status === "success"){
             toast('✔️ berhasil upload data', {
                 position: "top-right",
@@ -46,6 +52,7 @@ const Wilayah = () => {
                 progress:1,
                 theme: "light",
                 });
+            getData()
         } 
         else{
           toast('❌ gagal upload data', {
@@ -60,7 +67,35 @@ const Wilayah = () => {
         }
       }
 
-  
+    async function delete_wilayah(id_data){
+        const req = {
+            id:id_data
+        }
+        const send = await axios.delete("/api/dashboard/wilayah",{data:req})
+        if (send.data.status === "success"){
+            toast('✔️ berhasil menghapus data', {
+                position: "top-right",
+                autoClose: 0.1,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                progress:1,
+                theme: "light",
+                });
+            getData()
+        } 
+        else{
+          toast('❌ gagal menghapus data', {
+              position: "top-right",
+              autoClose: 0.1,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: false,
+              progress:1,
+              theme: "light",
+              });
+        }
+    }
     return (
         <section className="w-full h-auto lg:h-auto">
         <ToastContainer />
@@ -88,19 +123,12 @@ const Wilayah = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td class="border px-4 py-2">1</td>
-                            <td class="border px-4 py-2">Sukobumi</td>
-                            <td class="border px-4 py-2">
-                            {data != null ? (
+                         {data != null ? (
                             data.map((item,index) => (
-                                <tr className="xs:table-cell border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 " key={item.id}>
-                                    <td>{index+1}</td>
-                                    <td className="xs:table-cell border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">{item.nama}</td>
-                                </tr>
-                                )
-                                )
-                        ): null}
+                        <tr>
+                            <td class="border px-4 py-2">{index+1}</td>
+                            <td class="border px-4 py-2">{item.nama}</td>
+                            <td class="border px-4 py-2">
                             </td>
 
                             <td className="border flex justify-center p-2 gap-3">
@@ -114,13 +142,16 @@ const Wilayah = () => {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                     </svg>
                                 </button>
-                                <button type="" className="bg-red-400 px-2 py-1 rounded-md hover:bg-red-200 transition-all duration-300">
+                                <button onClick={(e)=>delete_wilayah(item.id)} type="" className="bg-red-400 px-2 py-1 rounded-md hover:bg-red-200 transition-all duration-300">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
                                 </button>
                             </td>
                         </tr>
+                            )
+                               )
+                            ): null} 
                     </tbody>
                 </table>
             </div>
