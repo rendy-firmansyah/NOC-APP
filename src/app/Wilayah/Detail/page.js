b5c91b8f008e229252294654a853031b7502cdb1
shrmanto <shrmantoch@gmail.com>
2024-01-22 01:06:23 +0700

    feat: dashboard detail survei umum && Kelompok

--- src/app/Wilayah/Detail/page.js
+++ src/app/Wilayah/Detail/page.js
@@ -89,12 +89,14 @@ const DetailWilayah = () => {
                                                     <td class="border text-center px-4 py-2">{item.anggota_keluarga[0].nama}</td>
                                                     <td class="border text-center px-4 py-2">{item.wilayah.nama}</td>
                                                     <td className="border flex justify-center p-2 gap-3">
-                                                        <button type="" className="bg-cyan-400 px-2 py-1 rounded-md flex items-center hover:bg-cyan-200 transition-all duration-300">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
-                                                            </svg>
-                                                            <span className="ms-2 text-sm">Cek Detail Kuisioner</span>
-                                                        </button>
+                                                        <Link href="/Wilayah/DetailKeluarga">
+                                                            <button type="" className="bg-cyan-400 px-2 py-1 rounded-md flex items-center hover:bg-cyan-200 transition-all duration-300">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                                                                </svg>
+                                                                <span className="ms-2 text-sm">Cek Detail Kuisioner</span>
+                                                            </button>
+                                                        </Link>
                                                     </td>
                                                 </tr>
                                                 ))
