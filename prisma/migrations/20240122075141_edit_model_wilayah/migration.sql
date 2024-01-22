90136f17ee8c538a850db63205b50398de143854
Yoga838 <212410102031@mail.unej.ac.id>
2024-01-22 15:01:50 +0700

    api layak or no

---
+++ prisma/migrations/20240122075141_edit_model_wilayah/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "wilayah" ADD COLUMN     "Deskripsi" TEXT,
+ADD COLUMN     "layak" TEXT;
---
+++ src/app/api/kelayakan/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server"
+import prisma from "../../../../lib/prisma";
+
+export async function POST(request: Request) {
+    const Data = await request.json()
+    try{
+        if(!Data.id || !Data.deskripsi){
+            return NextResponse.json({
+                status : "failed",
+                reason :"req body kosong"
+            })
+        }
+        const update = await prisma.wilayah.update({
+            where:{
+                id : Number(Data.id)
+            },
+            data:{
+                layak:true,
+                Deskripsi: Data.deskripsi
+            }
+        })
+
+        return NextResponse.json({
+            status :  "success"
+        })
+    }
+    catch(err){
+        return NextResponse.json({
+            status : "failed",
+            reason : err
+        })
+    }
+}
