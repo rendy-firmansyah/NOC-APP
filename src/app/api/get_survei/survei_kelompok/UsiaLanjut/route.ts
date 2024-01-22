793002ec348a61363918d4d15046c27f12fd3568
Yoga838 <212410102031@mail.unej.ac.id>
2024-01-22 12:00:16 +0700

    feat api get survei

---
+++ src/app/api/get_survei/survei_kelompok/UsiaLanjut/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server"
+import prisma from "../../../../../../lib/prisma";
+
+export async function POST(request: Request) {
+    const Data = await request.json()
+    try{
+        if(!Data.id){
+            return NextResponse.json({
+                status : "failed",
+                reason :"req body kosong"
+            })
+        }
+
+        const data = await prisma.kelompok_lansia.findFirst({
+            where:{
+                id_keluarga : Number(Data.id)
+            }
+        })
+        return NextResponse.json({
+            status:"success",
+            data
+        })
+
+    }
+
+    catch(err){
+        return NextResponse.json({
+            status : "failed",
+            reason : err
+        })
+    }
+}
